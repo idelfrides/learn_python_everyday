8564957984f6493b385ef1190ef24939c57133b0
idelfrides <idelfridesjorgepapai@gmail.com>
2022-02-18 09:45:18 -0300

    first commint of learn python everyday project

---
+++ main_app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#enconding: utf-9
+
+
+def run_main_app():
+
+    print('\n\n THIS IS THE MAIN MODULE OF THIS APP \n\n')
+
+    pass
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    run_main_app()
