d53f362fde9a579b8c8a50d72e5f5e87f6005b5f
idelfrides <idelfridesjorgepapai@gmail.com>
2022-02-19 09:49:12 -0300

    some changes

--- main_app.py
+++ main_app.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python
 #enconding: utf-9
 
+from webScraping.web_scraping import WebScraper
 
 def run_main_app():
 
     print('\n\n THIS IS THE MAIN MODULE OF THIS APP \n\n')
 
-    pass
+    scrap_obj = WebScraper()
+
+    # url = ''
+
+    scrap_obj.scraping_weather()
 
 
+    return
 
 
-
+# product_analysis
+# weather_analysis
+# climate_analysis#
+# descout_produt
+#
+#
+#
 
 
 
--- webScraping/web_scraping.py
+++ webScraping/web_scraping.py
@@ -1,12 +1,51 @@
 #!/usr/bin/python
 #enconding: utf-9
 
+from cgitb import html
+from socket import ntohl
+from bs4 import BeautifulSoup
+import requests
 
-class Scraping(object):
+class WebScraper(object):
 
     def __init__(self) -> None:
         pass
 
 
-    def web_scraping(self):
+    def scraping_weather(self, url=None, filename=None):
+       #  import pdb; pdb.set_trace()
+
+        if not url:
+            url = 'https://www.climatempo.com.br/'
+
+        if not filename:
+            filename = 'weather_scraper.text'
+
+        html = requests.get(url).text
+
+        # soup = BeautifulSoup(html, 'html.parser')
+        soup = BeautifulSoup(html, 'html.parser')
+
+        print(soup.prettify())
+
+
+        # print(f''' Teste formatacoao: {variavel} outra coisa: {variavel} ''')
+
+
+        # temperature = soup.find_all("iframe", class_='_block _margin-b-5 -gray')
+        # li_list = soup.find("div", class_='mosaic mosaic-provider-jobcards mosaic-provider-hydrated') #  class_='_block _margin-b-5 -gray')
+        # # import pdb; pdb.set_trace()
+#
+        # for li in li_list:
+        #     print('-'*80)
+        #     print(li)
+
+        # print(temperature.string)
+
+        return
+
+
+
+
+    def scraping_product(self):
         pass
