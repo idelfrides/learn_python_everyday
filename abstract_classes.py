4e53dac2291103ff4b71b8b1401e15398470dd4d
idelfrides <idelfridesjorgepapai@gmail.com>
2022-03-01 17:00:49 -0300

    create o module to lear aboude abstract classes in python

---
+++ abstract_classes.py
@@ -0,0 +1,50 @@
+'''
+---------------------------------------------------------------
+                CLASSES ABSTRATAS EM PYTHON
+---------------------------------------------------------------
+
+Trata-se de classes que define(cria) atributos e métodos, porém não os
+implementa.
+
+As classes abstratas são usadas para definir atributos  e ações que são implementadas/utilizadas em outras classes q são chmadas classes filha.
+
+São usadas em projetos onde vários componentes implementam o mesmo comportamento.
+
+Em Python uma classe é abstrata de ela tiver pelo menos  método abstrato.
+
+'''
+
+
+from abc import ABC, abstractmethod
+from shutil import move
+
+class Animal(ABC):
+
+    @abstractmethod
+    def move(self):
+        pass
+
+
+
+class Person(Animal):
+
+    def move(self):
+        print('\n I AM A MAN AND I CAN WALK \n')
+
+
+class Dog(Animal):
+
+    def move(self):
+        print('\n I AM A DOG AND I CAN LATIR')
+
+
+class Cat(Animal):
+
+    def move(self):
+        print('\n EU SOU GATO E EU POSSO MIAR')
+
+
+class Lion(Animal):
+
+    def move(self):
+        print('\n EU SOU UM LEÃO E POSSO RUGIR')
--- main_app.py
+++ main_app.py
@@ -2,6 +2,11 @@
 #enconding: utf-9
 
 from webScraping.web_scraping import WebScraper
+from abstract_classes import (
+    Person, Dog,
+    Cat, Lion
+)
+
 
 def run_main_app():
 
@@ -17,15 +22,30 @@ def run_main_app():
     return
 
 
+
 # product_analysis
 # weather_analysis
 # climate_analysis#
 # descout_produt
-#
-#
-#
+
+
+
+def run_abstract_class():
+    human = Person()
+    human.move()
+
+    dog = Dog()
+    dog.move()
+
+    cat = Cat()
+    cat.move()
+
+    lion = Lion()
+    lion.move()
+
 
 
 
 if __name__ == '__main__':
-    run_main_app()
+    # run_main_app()
+    run_abstract_class()
