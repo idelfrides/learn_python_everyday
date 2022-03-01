1300b4d0160dfd0601d75ff8801443fa72f53f50
idelfrides <idelfridesjorgepapai@gmail.com>
2022-03-01 17:32:37 -0300

    changes: add a attribute to abstract class

--- abstract_classes.py
+++ abstract_classes.py
@@ -6,18 +6,24 @@
 Trata-se de classes que define(cria) atributos e métodos, porém não os
 implementa.
 
-As classes abstratas são usadas para definir atributos  e ações que são implementadas/utilizadas em outras classes q são chmadas classes filha.
+As classes abstratas são usadas para definir atributos  e ações que são implementadas/utilizadas em outras classes que são achmadas classes filha.
 
-São usadas em projetos onde vários componentes implementam o mesmo comportamento.
+São usadas em projetos  de grande porte onde vários componentes implementam o mesma ação, ou seja, possuem o mesmo interface.
 
-Em Python uma classe é abstrata de ela tiver pelo menos  método abstrato.
+Python não possui define classes abstratas de maneira impirica, e sim possui um módulo base de classes abstratas chamado abc no qual é criado uma class base abstrata que é ABC.
+
+Em Python uma classe é abstrata se ela tiver pelo menos  método abstrato.
+
+um método abstrato quando é decorado com um método chamado
+@abstractmethod
 
 '''
 
 from abc import ABC, abstractmethod
-from shutil import move
 
 class Animal(ABC):
+
+    name_type = ''
 
     @abstractmethod
     def move(self):
@@ -25,24 +31,22 @@ class Animal(ABC):
 
 
 class Person(Animal):
-
+    name_type = ''
     def move(self):
-        print('\n I AM A MAN AND I CAN WALK \n')
-
+        print(f'\n {self.name_type} AND I CAN WALK \n')
 
 class Dog(Animal):
-
     def move(self):
         print('\n I AM A DOG AND I CAN LATIR')
 
-
 class Cat(Animal):
-
     def move(self):
         print('\n EU SOU GATO E EU POSSO MIAR')
 
-
 class Lion(Animal):
-
     def move(self):
         print('\n EU SOU UM LEÃO E POSSO RUGIR')
+
+class Eagle(Animal):
+    def move(self):
+        print('\n I AM A EAGLE AND I CAN MAKE A SOUND \n')
